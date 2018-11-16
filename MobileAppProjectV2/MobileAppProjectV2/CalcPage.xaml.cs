bac8b0b44f4c37ef3c6bd321338b3d5634b08511
EvanGreaney <g00348451@gmit.ie>
2018-11-16 10:23:20 +0000

    Created a new Repository due to errors with the old, added navigation through a tabbed view to the app

---
+++ MobileAppProjectV2/MobileAppProjectV2/CalcPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileAppProjectV2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalcPage : ContentPage
+    {
+        public CalcPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnEnter_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void btnEnter_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+        private void btnEnter_Clicked_3(object sender, EventArgs e)
+        {
+
+        }
+        private void btnEnter_Clicked_4(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
