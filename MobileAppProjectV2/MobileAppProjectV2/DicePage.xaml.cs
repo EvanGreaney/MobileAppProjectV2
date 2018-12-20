1d10a702a42676c10b285cf0cd9e99074d8fd294
EvanGreaney <g00348451@gmit.ie>
2018-12-20 00:35:19 +0000

    Added in Final Dice Roll, Fully functioning calculator and finished any loose ends to have the app fully completed

--- MobileAppProjectV2/MobileAppProjectV2/DicePage.xaml.cs
+++ MobileAppProjectV2/MobileAppProjectV2/DicePage.xaml.cs
@@ -16,5 +16,50 @@ namespace MobileAppProjectV2
 		{
 			InitializeComponent ();
 		}
-	}
+
+        //Dice Roll method that when called generates a random number
+        //and depending on the number displays a certain image of a dice
+        private void DiceRoller_Clicked(object sender, EventArgs e)
+        {
+            //random number generator
+            Random r = new Random();
+            int NumSelect = r.Next(0, 6);
+            if (NumSelect == 1)
+            {
+                DiceImage.Source = ImageSource.FromResource("MobileAppProjectV2.Images.inverted_dice_1.png");
+                Result.Text = "One";
+            }
+            else if (NumSelect == 2)
+            {
+                DiceImage.Source = ImageSource.FromResource("MobileAppProjectV2.Images.inverted_dice_2.png");
+                Result.Text = "Two";
+            }
+            else if (NumSelect == 3)
+            {
+                DiceImage.Source = ImageSource.FromResource("MobileAppProjectV2.Images.inverted_dice_3.png");
+                Result.Text = "Three";
+            }
+            else if (NumSelect == 4)
+            {
+                DiceImage.Source = ImageSource.FromResource("MobileAppProjectV2.Images.inverted_dice_4.png");
+                Result.Text = "Four";
+            }
+            else if (NumSelect == 5)
+            {
+                DiceImage.Source = ImageSource.FromResource("MobileAppProjectV2.Images.inverted_dice_5.png");
+                Result.Text = "Five";
+            }
+            else if (NumSelect == 6)
+            {
+                DiceImage.Source = ImageSource.FromResource("MobileAppProjectV2.Images.inverted_dice_6.png");
+                Result.Text = "Six";
+            }
+            else
+            {
+                DiceImage.Source = ImageSource.FromResource("MobileAppProjectV2.Images.inverted_dice_6.png");
+                Result.Text = "Six";
+
+            }
+        }
+    }
 }
